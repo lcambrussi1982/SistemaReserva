8db6d3a8c35c05ddd7d82b30f23d47797615533c
lcambrussi1982 <127839395+lcambrussi1982@users.noreply.github.com>
2025-11-15 18:00:41 -0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,13 +3,15 @@
 import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "COLOQUE_SUA_API_KEY_AQUI",
-  authDomain: "COLOQUE_SEU_AUTH_DOMAIN_AQUI",
+  apiKey: "AIzaSyAAQ92Zl69CHdMyvu22nM36M6ESxJ6BDOM",
+  authDomain: "reservacambrussi.firebaseapp.com",
   projectId: "reservacambrussi",
-  storageBucket: "COLOQUE_SEU_BUCKET_SE_PRECISAR",
-  messagingSenderId: "COLOQUE_SEU_SENDER_ID",
-  appId: "COLOQUE_SEU_APP_ID"
+  storageBucket: "reservacambrussi.firebasestorage.app",
+  messagingSenderId: "1079601388890",
+  appId: "1:1079601388890:web:e2dc83f7740c8544536763",
+  measurementId: "G-XGFJVQ67SM"
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
